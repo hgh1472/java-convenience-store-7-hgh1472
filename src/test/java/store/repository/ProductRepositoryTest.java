c25edd381a34812640c8d9218768a741434a0015
hgh1472 <hgh1472@naver.com>
2024-11-11 15:14:43 +0900

    test: 동일 상품 여러 프로모션 적용 테스트 코드 추가

---
+++ src/test/java/store/repository/ProductRepositoryTest.java
@@ -0,0 +1,36 @@
+package store.repository;
+
+import java.util.Optional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import store.dto.ProductInput;
+import store.dto.PromotionInput;
+import store.exception.DuplicatePromotionException;
+import store.model.Product;
+import store.model.Promotion;
+
+public class ProductRepositoryTest {
+    private ProductRepository productRepository;
+
+    @BeforeEach
+    public void setUp() {
+        productRepository = new ProductRepository();
+    }
+
+    @Test
+    @DisplayName("동일 상품에 여러 프로모션이 적용되지 않는다.")
+    public void duplicatePromotion() {
+        // GIVEN
+        ProductInput productInput = new ProductInput(new String[]{"콜라", "1000", "10", "프로모션"});
+        Promotion promotion = new Promotion(
+                new PromotionInput(new String[]{"프로모션", "2", "1", "2023-01-01", "2023-12-31"}));
+        productRepository.save(Product.of(productInput, Optional.of(promotion)));
+
+        // WHEN - THEN
+        Assertions.assertThatThrownBy(() -> productRepository.save(Product.of(productInput, Optional.of((promotion)))))
+                .isInstanceOf(
+                        DuplicatePromotionException.class);
+    }
+}
