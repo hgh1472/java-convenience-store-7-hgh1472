89af77d44f8574fd6fa13bc161cad33ef00be40b
hgh1472 <hgh1472@naver.com>
2024-11-11 16:39:55 +0900

    refactor: 상품 및 프로모션 데이터 등록 로직 리팩토링

--- src/main/java/store/view/Reader.java
+++ src/main/java/store/view/Reader.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import store.dto.data.ProductInput;
 import store.dto.data.PromotionInput;
 import store.exception.CustomIOException;
@@ -16,40 +17,36 @@ public class Reader {
     private static final String DELIMITER = ",";
 
     public List<PromotionInput> readPromotions() {
-        List<PromotionInput> promotionInputs = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(PROMOTION_PATH))) {
-            br.readLine();
-            while (true) {
-                String line = br.readLine();
-                if (line == null) {
-                    break;
-                }
-                String[] input = line.split(DELIMITER);
-                PromotionInput promotionInput = new PromotionInput(input);
-                promotionInputs.add(promotionInput);
-            }
+            skipHeader(br);
+            return readData(br, PromotionInput::new);
         } catch (IOException e) {
             throw new CustomIOException(e.getMessage());
         }
-        return promotionInputs;
     }
 
     public List<ProductInput> readProducts() {
-        List<ProductInput> productInputs = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(PRODUCT_PATH))) {
-            br.readLine();
-            while (true) {
-                String line = br.readLine();
-                if (line == null) {
-                    break;
-                }
-                String[] input = line.split(DELIMITER);
-                ProductInput productInput = new ProductInput(input);
-                productInputs.add(productInput);
-            }
+            skipHeader(br);
+            return readData(br, ProductInput::new);
         } catch (IOException e) {
             throw new CustomIOException(e.getMessage());
         }
-        return productInputs;
+    }
+
+    private void skipHeader(BufferedReader br) throws IOException {
+        br.readLine();
+    }
+
+    private <T> List<T> readData(BufferedReader br, Function<String[], T> mapper) throws IOException {
+        List<T> data = new ArrayList<>();
+        while (true) {
+            String line = br.readLine();
+            if (line == null) {
+                break;
+            }
+            data.add(mapper.apply(line.split(DELIMITER)));
+        }
+        return data;
     }
 }
